5e1b19dd15a05284958975fc2cb81234857a5b3f
vern-gora <103502360+vern-gora@users.noreply.github.com>
2023-08-30 01:10:32 +0300

    Initial commit

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+const Statistics = ({ onGood, onNeutral, onBad, onTotal, onPositive }) => {
+  return (
+    <>
+      <p className={css.p}>
+        good: <span>{onGood}</span>
+      </p>
+      <p className={css.p}>
+        neutral: <span>{onNeutral}</span>
+      </p>
+      <p className={css.p}>
+        bad: <span>{onBad}</span>
+      </p>
+      <p className={css.p}>
+        total: <span>{onTotal}</span>
+      </p>
+      <p className={css.p}>
+        positive feedback:{' '}
+        <span>
+          <b>{onPositive}%</b>
+        </span>
+      </p>
+    </>
+  );
+};
+
+Statistics.propTypes = {
+  onGood: PropTypes.number.isRequired,
+  onNeutral: PropTypes.number.isRequired,
+  onBad: PropTypes.number.isRequired,
+  onTotal: PropTypes.number.isRequired,
+  onPositive: PropTypes.number.isRequired,
+};
+
+export default Statistics;
